ba611848661ccaaf9356c410ab03dac826124a39
Lawrence Zheng <zheng3@hawaii.edu>
2026-07-10 10:57:46 -1000

    FInally made a rudimentary bash script to set the RT optimizations

---
+++ rt_on.bash
@@ -0,0 +1,40 @@
+#! /usr/bin/env bash
+echo "Setting CPU governor to performance mode"
+echo -n performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+
+echo "Turning off SMT"
+echo off | sudo tee /sys/devices/system/cpu/smt/control
+
+echo "Setting swapiness to 3"
+sudo sysctl vm.swappiness=3
+
+echo "Locking USB IRQ to cpu 12"
+echo 12 | sudo tee /proc/irq/39/smp_affinity_list 
+
+echo "Disabling C2 and C3 states"
+for cpu in /sys/devices/system/cpu/cpu[0-9]*; do
+echo 1 | sudo tee "$cpu/cpuidle/state2/disable" >/dev/null
+echo 1 | sudo tee "$cpu/cpuidle/state3/disable" >/dev/null
+done 
+
+echo "double check pls"
+
+echo "CPU governor:"
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor 
+
+echo "SMT:"
+cat /sys/devices/system/cpu/smt/control
+
+echo "Swapiness:"
+cat /proc/sys/vm/swappiness
+
+echo "USB IRQ:"
+cat /proc/irq/39/smp_affinity_list
+
+echo "C2 and C3 states:"
+for cpu in /sys/devices/system/cpu/cpu[0-9]*; do
+echo "$cpu"
+paste $cpu/cpuidle/state*/name $cpu/cpuidle/state*/disable 2>/dev/null
+done 
+
+
